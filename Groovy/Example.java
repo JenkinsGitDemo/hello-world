d9be1ba103085398e3cfc65952954884e22c0e47
JenkinsGitDemo <write2ancy@gmail.com>
2017-04-06 18:15:17 +0530

    Testing a Sample Addition

---
+++ Groovy/Example.java
@@ -0,0 +1,6 @@
+class Example {
+   public static void main(String[] args) {
+      // Using a simple println statement to print output to the console
+      System.out.println("Hello World");
+   }
+}
